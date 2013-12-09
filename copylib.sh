e3999b62d0301dbed50667de11a4438fc4e780fe
Tom Jacques <tejacques@gmail.com>
2013-12-09 16:01:02 -0500

    Update NuGet packages

--- copylib.sh
+++ copylib.sh
@@ -1,4 +1,4 @@
 cp src/AsyncBridge/bin/Release/AsyncBridge.* lib/AsyncBridge/NET45/
 cp src/AsyncBridge.NET40/bin/Release/AsyncBridge.NET40* lib/AsyncBridge/NET40/
-cp src/AsyncBridge.Option/bin/Release/AsyncBridge.Option.* lib/AsyncBridge.Option/NET45/
-cp src/AsyncBridge.NET40.Option/bin/Release/AsyncBridge.NET40.Option* lib/AsyncBridge.Option/NET40/
+#cp src/AsyncBridge.Option/bin/Release/AsyncBridge.Option.* lib/AsyncBridge.Option/NET45/
+#cp src/AsyncBridge.NET40.Option/bin/Release/AsyncBridge.NET40.Option* lib/AsyncBridge.Option/NET40/
